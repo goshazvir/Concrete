11bf75556922a45a39ec86545dd1d042086d39c8
Georgiy <goshazvir@gmail.com>
2015-02-21 14:25:07 +0200

    mobile

--- js/common.js
+++ js/common.js
@@ -18,13 +18,49 @@ head.ready(function() {
 			});
 	} tab();
 
-	$('.js-slick').slick({
-		centerMode: true,
-		centerPadding: '0px',
-	  slidesToShow: 3,
-	  slidesToScroll: 1,
-	  autoplay: true,
-	  autoplaySpeed: 20000,
+	$('.js-nav-open').on('click', function() {
+		$(".nav ul").show();
+		$("body").addClass("is-overlay");
 	});
+	$('.js-nav-close').on('click', function() {
+		$(this).parents(".nav ul").hide();
+		$("body").removeClass("is-overlay");
+	});
+
+	function slick() {
+		$('.js-slick').slick({
+			centerMode: true,
+			centerPadding: '0px',
+		  slidesToShow: 3,
+		  slidesToScroll: 1,
+		  autoplay: true,
+		  autoplaySpeed: 500000,
+		  responsive: [
+		  {
+		    breakpoint: 980,
+		    settings: {
+		      slidesToShow: 3,
+		      slidesToScroll: 1
+		    }
+		  },
+		     {
+		       breakpoint: 500,
+		       settings: {
+		         slidesToShow: 1,
+		         slidesToScroll: 1
+		       }
+		     }
+		   ]
+		});
+	}
+	slick()
+
+
+
+
+	$(window).resize(function(){
+		slick()
+	});
+
 
 });
